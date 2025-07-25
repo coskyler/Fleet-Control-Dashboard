d0be63d4a98832e5fe25d56d289b53b3fdc58285
Skyler Quinby <skylerpquinby@gmail.com>
2025-07-25 14:50:22 -0400

    integrated postgres, express-session, and redis for database and session management

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const session = require('express-session');
+const RedisStore = require('connect-redis').default;
+const { createClient } = require('redis');
+const pool = require('./db');
+
 const app = express();
 const PORT = 3000;
+
+const redisClient = createClient({url: 'redis://localhost:6379'});
+redisClient.connect().catch(console.error);
+
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: process.env.SESSION_SIGNATURE,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, maxAge: 86400000 } // 1 day
+  })
+);
 
 app.get('/', (req, res) => {
   res.send('Backend is running!');
