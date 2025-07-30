96cef0a39f920f66ce477c5e7a096584e4987f73
Skyler Quinby <skylerpquinby@gmail.com>
2025-07-30 18:16:45 -0400

    Add buttons for start/stop/dispatch/recall

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -12,8 +12,16 @@ export default function Dashboard() {
                 </div>
             </section>
 
-            <section className="h-20 bg-neutral-700 mt-4">
+            <section className="flex mt-4">
+                <div className="w-64 flex flex-col gap-2">
+                    <button className="w-fit font-semibold bg-green-700 px-3 py-1 rounded hover:bg-green-800 transition duration-200 active:bg-green-700">Start Scan</button>
+                    <button className="w-fit font-semibold bg-red-800 px-3 py-1 rounded hover:bg-red-900 transition duration-200 active:bg-red-800">End Scan</button>
+                </div>
 
+                <div className="flex-1 flex justify-center gap-4 pb-6">
+                    <button className="w-fit font-semibold bg-neutral-600 px-3 py-1 rounded hover:bg-neutral-700 transition duration-200 active:bg-neutral-600">Dispatch</button>
+                    <button className="w-fit font-semibold bg-neutral-600 px-3 py-1 rounded hover:bg-neutral-700 transition duration-200 active:bg-neutral-600">Recall</button>
+                </div>
             </section>
         </main>
     );
