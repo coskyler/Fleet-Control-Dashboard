b9f1aad97108f07be9da0dfa0d43f84278f4638c
Skyler Quinby <skylerpquinby@gmail.com>
2025-09-12 02:33:32 -0400

    Move db.js into src, being refactoring index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 
 const app = express();
-app.get("/", (req, res) => res.send("hello World!"));
+app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(80, () => console.log("Container listening on port 80!"));
--- backend/src/oldindex.js
+++ backend/src/oldindex.js
@@ -2,7 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const RedisStore = require('connect-redis').default;
 const { createClient } = require('redis');
-const pool = require('../db');
+const pool = require('./db');
 
 const app = express();
 const PORT = 3000;
