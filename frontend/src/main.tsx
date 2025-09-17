53064b8d81287088a6850de233e42907ac4cc0bd
Skyler Quinby <skylerpquinby@gmail.com>
2025-09-16 20:08:22 -0400

    Fix reference in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter} from 'react-router-dom';
 import './index.css'
 import App from './App.tsx'
-import { WebSocketProvider } from './websocket/WebSocketContext.tsx';
+import { WsProvider } from './websocket/ScanWsContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <WebSocketProvider>
+      <WsProvider>
         <App />
-      </WebSocketProvider>
+      </WsProvider>
     </BrowserRouter>
   </StrictMode>
 )
