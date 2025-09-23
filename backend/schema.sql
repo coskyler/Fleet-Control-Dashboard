d5c8917ba62280d3c1e23602984205fd85f03349
Skyler Quinby <skylerpquinby@gmail.com>
2025-09-23 15:50:02 -0400

    Create schema.sql

---
+++ backend/schema.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE fleet_control_dashboard_db;
+
+\c fleet_control_dashboard_db;
+
+CREATE TABLE IF NOT EXISTS users (
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS scans (
+    scan_id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    name VARCHAR(50) NOT NULL DEFAULT 'Untitled Scan',
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    voxels INT[] NOT NULL,
+    CHECK (mod(coalesce(array_length(voxels, 1),0), 3) = 0) --ensure voxels is a multiple of 3 (representing vector3s)
+);
+
+CREATE INDEX IF NOT EXISTS idx_user_scans ON scans(user_id, created_at DESC);
